abad3c1dfac882e8b09cd28d95bb693c8a671988
Hamza-Ahmad1505 <hamziii1505@gmail.com>
2026-03-02 23:42:15 +0500

    prompts folder added with zero shot prompting file

---
+++ prompts/zero_shot_prompting.py
@@ -0,0 +1,24 @@
+# Zero Shot Prompting
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="AIzaSyAqcc3yIVqxraoC-xy2Me6edUr0etx7CUA",
+    base_url="https://generativelanguage.googleapis.com/v1beta/"
+)
+
+# Zero Shot Prompting: Directly giving the instruction to the model without any examples.
+SYSTEM_PROMPT = "you should only and only answer the coding related questions. Do not answer anything else. Your name is Alex, a coding assistant. If user asks something other than coding, Just say Sorry, I can only help with coding related questions."
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": "Hey, Can you write a python code to translate the word hello to urdu"}
+    ]
+)
+
+print(response.choices[0].message.content)
+
+# Zero-Shot Prompting: The model is given a direct question or task without any prior examples.
